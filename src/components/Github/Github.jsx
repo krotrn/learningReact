5ffad2df1ff6a0bca5ecc7fc5e103e3d7ff9e541
krotrn <karankumarsahtkg2018@gmail.com>
2024-09-30 03:23:30 +0530

    Refactor code and remove unused useState hook in App.jsx

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -12,24 +12,24 @@ function Github() {
     //             setData(data)
     //         })
     // }, [])
-    
+
     const data = useLoaderData()
 
-  return (
-      <>
-          <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
-              Github Follower : {data.followers}
-          <img src={data.avatar_url} alt="Git Picture" width={300} />
-          </div>
-      </>
-      
-  )
+    return (
+        <>
+            <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+                Github Follower : {data.followers}
+                <img src={data.avatar_url} alt="Git Picture" width={300} />
+            </div>
+        </>
+
+    )
 }
 
 export default Github
 
 export const githubInfoLoader = async () => {
     const response = await fetch(`https://api.github.com/users/krotrn`)
-            .then(response => response.json())
+        .then(response => response.json())
     return response
 }
